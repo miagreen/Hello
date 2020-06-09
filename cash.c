9693fba3034dea6095e838b5b5b37cff59d7e1ef
Miagreen <evevevev75757575@gmail.com>
2020-06-09 16:25:37 +0000

    :fire: Remove codes

--- cash.c
+++ cash.c
@@ -12,27 +12,7 @@ int main()
     int count = 0;
     while(cents > 0)
     {
-        if(cents >= 25)
-        {
-            count = count + (cents/25);
-            cents = cents % 25;
-        }
-        else if(cents >= 10)
-        {
-            count = count + (cents / 10);
-            cents = cents % 10;
-        }
-        else if (cents >= 5)
-        {
-            count = count + (cents / 5);
-            cents = cents % 5;
-        }
-        else if(cents >=1)
-        {
-            count = count + (cents / 1);
-            cents = cents % 1;
-        }
-        // printf("%d\n", cents);
+
     }
     printf("%d\n", count);
     return 0;
