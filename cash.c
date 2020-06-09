b32dab7f19e4060493b64b30121d15bec147e9cd
Miagreen <evevevev75757575@gmail.com>
2020-06-09 16:29:43 +0000

    :recycle: Remove While

--- cash.c
+++ cash.c
@@ -10,9 +10,25 @@ int main()
     }while(dollars < 0);
     int cents = round(dollars * 100);
     int count = 0;
-    while(cents > 0)
+    if(cents >= 25)
     {
-
+        count = count + (cents/25);
+        cents = cents % 25;
+    }
+    if(cents >= 10)
+    {
+        count = count + (cents / 10);
+        cents = cents % 10;
+    }
+    if (cents >= 5)
+    {
+        count = count + (cents / 5);
+        cents = cents % 5;
+    }
+    if(cents >=1)
+    {
+        count = count + (cents / 1);
+        cents = cents % 1;
     }
     printf("%d\n", count);
     return 0;
